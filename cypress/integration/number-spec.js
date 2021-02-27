cdb4ce385ab1750ea8e8919404a3dee851c2c535
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-27 16:02:19 -0500

    more examples

--- cypress/integration/number-spec.js
+++ cypress/integration/number-spec.js
@@ -47,3 +47,74 @@ it('yields 7 from the task (recursive)', () => {
     (n) => n === 7,
   )
 })
+
+it('yields 7 from the task (iteration count)', () => {
+  const recurse = (commandsFn, checkFn, k = 1) => {
+    cy.log(`iteration **${k}**`)
+
+    commandsFn().then((x) => {
+      if (checkFn(x)) {
+        cy.log('**NICE!**')
+        return
+      }
+
+      recurse(commandsFn, checkFn, k + 1)
+    })
+  }
+
+  recurse(
+    () => cy.task('randomNumber'),
+    (n) => n === 7,
+  )
+})
+
+it('yields 7 from the task (limit)', () => {
+  const recurse = (commandsFn, checkFn, limit = 3) => {
+    if (limit < 0) {
+      throw new Error('Recursion limit reached')
+    }
+    cy.log(`remaining attempts **${limit}**`)
+
+    commandsFn().then((x) => {
+      if (checkFn(x)) {
+        cy.log('**NICE!**')
+        return
+      }
+
+      recurse(commandsFn, checkFn, limit - 1)
+    })
+  }
+
+  recurse(
+    () => cy.task('randomNumber'),
+    (n) => n === 7,
+    30,
+  )
+})
+
+it('yields 7 from the task (time limit)', () => {
+  const recurse = (commandsFn, checkFn, timeRemaining = 4000) => {
+    const started = +new Date()
+    if (timeRemaining < 0) {
+      throw new Error('Max time limit reached')
+    }
+    cy.log(`time remaining **${timeRemaining}**`)
+
+    commandsFn().then((x) => {
+      if (checkFn(x)) {
+        cy.log('**NICE!**')
+        return
+      }
+
+      const finished = +new Date()
+      const elapsed = finished - started
+      recurse(commandsFn, checkFn, timeRemaining - elapsed)
+    })
+  }
+
+  recurse(
+    () => cy.task('randomNumber'),
+    (n) => n === 7,
+    10000,
+  )
+})
